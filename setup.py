d65a21b35224a2ba7252300c21f726aac9fd7e26
Kugalus <galanus2001@gmail.com>
2022-03-24 16:00:23 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="chuj",
+    version='0.1',
+    description='to jest kurwa biblioteka',
+    url='',
+    author='josif stalin',
+    author_email='josifstalin@microsoft.com',
+    license='unlicense',
+    packages=['biblbio'],
+    zip_safe=False
+)
